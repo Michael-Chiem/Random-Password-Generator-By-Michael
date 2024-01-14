bcf009f515819762f61d64d25c98b28c1b8dd1a5
Michael Chiem <michaelchiem1@hotmail.com>
2024-01-14 16:43:39 -0700

    Adding comments inside the JS file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// 1) Refactor the code by establishing variables to capture the end-user's selection and implementing a for-loop function.
 var numberLength = 8;
 var choiceArray = [];
 
@@ -11,15 +13,19 @@ var lowerCaseCharacterArray = lowerCaseCharacters.split('');
 var upperCaseCharacterArray = upperCaseCharacters.split('');
 var numberCharacterArray = numberCharacters.split('');
 
-
+// 2) n this step, we establish a path or location using the Document Object Model (DOM).
 var generateBtn = document.querySelector("#generate");
 
+
+// 3) apply a click event listener to trigger the activation of the popUpScreen function.
 generateBtn.addEventListener("click", function() {
   popUPScreen();
   writePassword();
 });
 
 
+// 5) The values obtained in step 4 are now being passed to the generatePassword function in step 6.
+// 7) The output from step 6 is now inserted into the HTML body, showcasing the result to the end-user.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -29,8 +35,11 @@ function writePassword() {
   passwordText.classList.add("flashing");
 }
 
+// 4) Upon activation of this function, it employs a for-loop parameter and checks for Not-A-Number to validate user input, subsequently concatenating the input value with the variables.
 function popUPScreen () {
   choiceArray = [];
+
+  // Note: I introduced the incorrectAttempts parameter to alert end-users when they have entered incorrect values within three attempts.
   var incorrectAttempts = 0;
 
   function promptUser() {
@@ -69,6 +78,7 @@ promptUser();
 
 }
 
+// 6) It initiates the generation of a random password according to the end-user's preferences, subsequently passing it to step 7.
 function generatePassword() {
   var password = "";
   for (var i = 0; i < numberLength; i++) {
