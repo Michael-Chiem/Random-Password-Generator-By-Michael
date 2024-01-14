08a4bbded020151a7bdf6ee3633aaf5b56ae7643
Michael Chiem <michaelchiem1@hotmail.com>
2024-01-13 18:04:10 -0700

    I updated the all the characters variable and arrays so that I can easily read the file structure

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,16 @@
 var characterLength = 8;
 var choiceArray = [];
 
-var specialCharacterArray = ['!','@','#','$','%','^','&','*','(',')','{','}','[',']','/',':',';'];
-var lowerCaseCharacterArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCaseCharacterArray = ['A','B','C','D','E','F','G','H','I','J','K','I','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numberCharacterArray = ['1','2','3','4','5','6','7','8','9','0'];
+var specialCharacters = '!@#$%^&*(){}[]/:;';
+var lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numberCharacters = '0123456789';
+
+var specialCharacterArray = specialCharacters.split('');
+var lowerCaseCharacterArray = lowerCaseCharacters.split('');
+var upperCaseCharacterArray = upperCaseCharacters.split('');
+var numberCharacterArray = numberCharacters.split('');
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
